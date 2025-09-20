d068d77f3be042c1cef4963a93676fa5d2c6093e
Ahsan Iqbal <mianahson@gmail.com>
2025-09-20 23:58:25 +0200

    tiling works

--- src/custom_ops/matmul/kernels.cu
+++ src/custom_ops/matmul/kernels.cu
@@ -3,16 +3,18 @@
 #include <ATen/Dispatch.h>
 #include "op.hpp"
 
-__device__ int getBatchOffset(const int* __restrict__ stridesOut,
-                              const int* __restrict__ stridesIn, 
-                              const int* __restrict__ shapeIn,
-                              int linearIndexOut, int rank) {
+#define TILE_WIDTH 16 
+
+__device__ int getBatchOffset(int batchIndex, int rank, 
+                                const int* __restrict__ strides,
+                                const int* __restrict__ shape,
+                                const int* __restrict__ shapeBroadcast) {
     int batchOffset = 0;
     int dimIndex = 0;
-    for (int i = 0; i < (rank - 2); i++) {
-        dimIndex = linearIndexOut / stridesOut[i];
-        linearIndexOut %= stridesOut[i];
-        batchOffset += (shapeIn[i] == 1 ? 0 : dimIndex) * stridesIn[i];
+    for (int i = rank - 3; i >= 0; i--) {
+        dimIndex = batchIndex % shapeBroadcast[i];
+        batchIndex /= shapeBroadcast[i];
+        batchOffset += (shape[i] == 1 ? 0 : dimIndex) * strides[i];
     }
     return batchOffset;
 }
@@ -22,40 +24,71 @@ template<typename scalar_t>
 __global__ void cmatmul_forward_kernel(const scalar_t* __restrict__ inputA,
     const scalar_t* __restrict__ inputB, scalar_t* __restrict__ output,
     const int* __restrict__ stridesA, const int* __restrict__ stridesB,
-    const int* __restrict__ stridesOut, const int* __restrict__ shapeA,
+    const int* __restrict__ shapeOut, const int* __restrict__ shapeA,
     const int* __restrict__ shapeB, int rank) {
 
-        int batch = blockIdx.z;
-        int rowIndex = blockIdx.y * blockDim.y + threadIdx.y; 
-        int colIndex = blockIdx.x * blockDim.x + threadIdx.x;
+    int batchIndex = blockIdx.z;
+    int rowIndex = blockIdx.y * blockDim.y + threadIdx.y; 
+    int colIndex = blockIdx.x * blockDim.x + threadIdx.x;
 
-        int rowsA = shapeA[rank - 2];
-        int colsA = shapeA[rank - 1];
+    int rowsA = shapeA[rank - 2];
+    int colsA = shapeA[rank - 1];
+    int colsB = shapeB[rank - 1];
 
-        int colsB = shapeB[rank - 1];
+    __shared__ scalar_t sharedDataA[TILE_WIDTH * TILE_WIDTH];
+    __shared__ scalar_t sharedDataB[TILE_WIDTH * TILE_WIDTH];
 
-        if (rowIndex < rowsA && colIndex < colsB) {
-            auto indexOut = batch * rowsA * colsB + rowIndex * colsB + colIndex;
+    int numPhases = (colsA + TILE_WIDTH - 1) / TILE_WIDTH;
 
-            auto batchOffsetA = getBatchOffset(stridesOut, stridesA, shapeA, indexOut, rank);
-            auto batchOffsetB = getBatchOffset(stridesOut, stridesB, shapeB, indexOut, rank);
+    auto batchOffsetA = getBatchOffset(batchIndex, rank, stridesA, shapeA, shapeOut);
+    auto batchOffsetB = getBatchOffset(batchIndex, rank, stridesB, shapeB, shapeOut);
 
-            auto outValue = scalar_t(0);
-            for (int i = 0; i < colsA; i++) {
-                auto indexA = batchOffsetA + rowIndex * stridesA[rank - 2] + i * stridesA[rank - 1];
-                auto indexB = batchOffsetB + i * stridesB[rank - 2] + colIndex * stridesB[rank - 1];
+    int indexSharedData = threadIdx.y * TILE_WIDTH + threadIdx.x;
+    scalar_t outValue = scalar_t(0);
 
-                outValue += inputA[indexA] * inputB[indexB];
-            }
-            output[indexOut] = outValue;
+    int colInTile = 0.0;
+    int rowInTile = 0.0;
+
+    for (int phase = 0; phase < numPhases; phase++) {
+        colInTile = phase * TILE_WIDTH + threadIdx.x;
+        rowInTile = phase * TILE_WIDTH + threadIdx.y;
+        
+        if (rowIndex < rowsA && colInTile < colsA) {
+            sharedDataA[indexSharedData] = 
+                inputA[batchOffsetA + rowIndex * stridesA[rank - 2] + colInTile * stridesA[rank - 1]];
         }
+        else {
+            sharedDataA[indexSharedData] = scalar_t(0);
+        }
+
+        if (colIndex < colsB && rowInTile < colsA) {
+            sharedDataB[indexSharedData] = 
+                inputB[batchOffsetB + rowInTile * stridesB[rank - 2] + colIndex * stridesB[rank - 1]];
+        }
+        else {
+            sharedDataB[indexSharedData] = scalar_t(0);
+        }   
+
+        __syncthreads();
+
+        for(int i = 0; i < TILE_WIDTH; i++) {
+            outValue += sharedDataA[threadIdx.y * TILE_WIDTH + i] * sharedDataB[i * TILE_WIDTH + threadIdx.x];
+        }
+        __syncthreads();
+    }
+    if (rowIndex < rowsA && colIndex < colsB) {
+        output[batchIndex * rowsA * colsB + rowIndex * colsB + colIndex] = outValue;
+    }
 }
 
 at::Tensor torchlab::ops::matmul::unsqueezeToDim(const at::Tensor& t, int targetDim) {
     TORCH_CHECK(t.dim() <= targetDim,
     "expected input tensor to have at most ", targetDim, " dims, got ", t.dim());
 
-    if (t.dim() == targetDim) {
+    if (t.dim() == targetDim) {he same device");
+
+    TORCH_CHECK(inputA.layout() == at::kStrided, "cmatmul::forward inputA must be a strided Tensor.");
+    TORCH_CHECK(inputB.layout() == at::kStrided, "cmat
         return t;
     }
 
@@ -137,26 +170,27 @@ at::Tensor torchlab::ops::matmul::forward(const at::Tensor& inputA, const at::Te
     // depending on the tensor sizes.
     auto stridesA = at::tensor(matA.strides(), matA.options().dtype(c10::kInt));
     auto stridesB = at::tensor(matB.strides(), matB.options().dtype(c10::kInt));
-    auto stridesOut = at::tensor(output.strides(), output.options().dtype(c10::kInt));
+
+    auto shapeOut = at::tensor(output.sizes(), output.options().dtype(c10::kInt));
     auto shapeA = at::tensor(matA.sizes(), matA.options().dtype(c10::kInt));
     auto shapeB = at::tensor(matB.sizes(), matB.options().dtype(c10::kInt));
 
     //kernel call
     AT_DISPATCH_FLOATING_TYPES(matA.scalar_type(), "cmatmul::forward", [&]{
-        int numThreadsRows = 32;
-        int numThreadsCols = 32;
+        //int numThreadsRows = 32;
+        //int numThreadsCols = 32;
 
         int batchCount = std::accumulate(outputShape.begin(), outputShape.end() - 2, int(1), std::multiplies<int>());
 
-        dim3 gridConfig = dim3((outputShape[outputShape.size() - 1] + numThreadsCols - 1) / numThreadsCols,
-            (outputShape[outputShape.size() - 2] + numThreadsRows - 1) / numThreadsRows,
+        dim3 gridConfig = dim3((outputShape[outputShape.size() - 1] + TILE_WIDTH - 1) / TILE_WIDTH,
+            (outputShape[outputShape.size() - 2] + TILE_WIDTH - 1) / TILE_WIDTH,
             batchCount);
-        dim3 blockConfig = dim3(numThreadsRows, numThreadsCols, 1);
+        dim3 blockConfig = dim3(TILE_WIDTH, TILE_WIDTH, 1);
 
         //kernel_launch_here
         cmatmul_forward_kernel<scalar_t><<<gridConfig, blockConfig, 0, stream>>>(
             matA.data_ptr<scalar_t>(), matB.data_ptr<scalar_t>(), output.data_ptr<scalar_t>(),
-            stridesA.data_ptr<int>(), stridesB.data_ptr<int>(), stridesOut.data_ptr<int>(),
+            stridesA.data_ptr<int>(), stridesB.data_ptr<int>(), shapeOut.data_ptr<int>(),
             shapeA.data_ptr<int>(), shapeB.data_ptr<int>(),  static_cast<int>(output.dim()));
 
         C10_CUDA_KERNEL_LAUNCH_CHECK();
